5c0fd80e54a6f11b51597f8f019c94d38c53dae5
eyadras98 <eyadras1998@gmail.com>
2025-11-22 11:10:33 +0300

    git commit -m "Ignore Debug and .vs folders"

---
+++ IdentityService.Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityService.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public ICollection<UserRole> Roles { get; set; }
+    }
+
+}
